eb195e3aac22556bb9f3365d509e75f548e1e96f
Arthur Hofmann <arthur.hofmann89@gmail.com>
2024-10-28 11:43:33 +0100

    update

--- pdo.php
+++ pdo.php
@@ -4,6 +4,7 @@
 // contection to database
 try{
   $pdo = new PDO('mysql:host=localhost;dbname=users;charset=utf8', 'testus', '12345');
+  $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 }
 
 catch (Exception $e){
