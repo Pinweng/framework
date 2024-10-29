e225a18181c6f05f23febfb8ae3f857356fe5529
Arthur Hofmann <arthur.hofmann89@gmail.com>
2024-10-29 16:47:07 +0100

    update UserController with new function pageload

--- mainsrc/User/MVC/UserController.php
+++ mainsrc/User/MVC/UserController.php
@@ -11,11 +11,17 @@ class UserController{
     $this->userDatabase = $userDatabase;
   }
 
+  public function pageload($variblen){
+    $user = $variblen["user"];
+    require_once "Views/user.php";
+  }
+
   public function userprofile($userid){
     $user = $this->userDatabase->getUser($userid);
 
-    require_once "Views/user.php";
-
+    $this->pageload([
+      "user" =>$user
+    ]);
   }
   
 }
